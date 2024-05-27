bde7d793521c4c6d1fd10b8461944984c3e791b1
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-27 13:43:30 -0500

    Add files via upload
    Starting project...

---
+++ 100_Days_Day_10.py
@@ -0,0 +1,18 @@
+##############
+# Calculator #
+##############
+
+print("It's time to use the calculator!\n")
+
+input("What's the first number? \n")
+print("+")
+print("-")
+print("*")
+print("/")
+input("Pick an operation: \n")
+input("What's the next number? \n")
+print("5 / 2 = 2.5")
+input("Type \"y\" to continue calculating with 2.5, or type \"n\" to start a new calculation: \n")
+
+print()
+print("Thanks for using the calculator!")
