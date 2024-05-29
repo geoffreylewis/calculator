b4a89eee9165f4d0ae9662cd7c3d2565d9e1e45c
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-29 11:32:44 -0500

    Add files via upload
    Created a simple calculate() function with if/elif statements based on operators.

--- 100_Days_Day_10.py
+++ 100_Days_Day_10.py
@@ -4,15 +4,30 @@
 
 print("It's time to use the calculator!\n")
 
-input("What's the first number? \n")
+def calculate(first_num, optr, next_num):
+    if optr == "+":
+        return first_num + next_num
+    elif optr == "-":
+        return first_num - next_num
+    elif optr == "*":
+        return first_num * next_num
+    elif optr == "/":
+        return first_num / next_num
+
+first_number = int(input("What's the first number? \n"))
+print()
 print("+")
 print("-")
 print("*")
 print("/")
-input("Pick an operation: \n")
-input("What's the next number? \n")
-print("5 / 2 = 2.5")
-input("Type \"y\" to continue calculating with 2.5, or type \"n\" to start a new calculation: \n")
+print()
+operation = input("Pick an operation: \n")
+print()
+next_number = int(input("What's the next number? \n"))
+calculation = calculate(first_num = first_number, optr = operation, next_num = next_number)
+print(calculation) # TEST CODE
+# print("5 / 2 = 2.5")
+# input("Type \"y\" to continue calculating with 2.5, or type \"n\" to start a new calculation: \n")
 
 print()
 print("Thanks for using the calculator!")
